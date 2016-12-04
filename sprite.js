c35e876925655836459678498117a4320e95cbca
Andre Nanninga <ag.nanninga@gmail.com>
2016-12-04 20:59:10 +0100

    added more items

--- sprite.js
+++ sprite.js
@@ -4,18 +4,18 @@ nsg({
 	layout: 'packed',
 	compositor: 'jimp',
 	src: ['items/*.png'],
-	spritePath: 'web/assets/items.png',
-	stylesheetPath: 'web/assets/items.css',
+	spritePath: 'docs/assets/items.png',
+	stylesheetPath: 'docs/assets/items.css',
 	stylesheet: 'css',
 	stylesheetOptions: {
 		prefix: 'item-',
-		nameMapping: (name) => name
-			.slice(6)
-			.slice(0, -4)
-			.toLowerCase()
-			.replace(/ /g, '-')
-			.replace(/[\',\(\)&]/g, '')
-			.replace(/--/g, '-')
+		nameMapping: (name) =>  name
+				.slice(6)
+				.slice(0, -4)
+				.toLowerCase()
+				.replace(/ /g, '-')
+				.replace(/[\',\(\)&\.]/g, '')
+				.replace(/--/g, '-')
 	},
 }, (err) => {
 	console.log(err, 'node-sprite-generator done');
